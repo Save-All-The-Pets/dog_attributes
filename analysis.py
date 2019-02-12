28428ccd3ce5d4f99a080a4c9e92f41cce53e8b8
Michael <msuttles@gmail.com>
2019-02-12 12:27:56 -0600

    Added more on analysis; cleaned data

--- analysis.py
+++ analysis.py
@@ -4,8 +4,33 @@ from pprint import pprint
 import sys
 import pandas as pd
 
-nyc_registry = pd.read_csv('NYC_Dog_Licensing_Dataset.csv')
-iq = pd.read_csv('dog_intelligence.csv')
-nyc_census = pd.read_csv('ACS_17_1YR_S0201_with_ann-edit.csv') # use 2016 data
-edmonton_registry = pd.read_csv('Edmonton_Pet_Licenses_by_Neighbourhood.csv')
-adelaide_registry = pd.read_csv('Dog_Registrations_Adelaide_2016.csv')
+client = MongoClient('localhost', 27017)
+# Access/Initiate Database
+db = client['dogs']
+# Access/Initiate Table
+data = db['data']
+
+nyc_registry = pd.read_csv('dogdata/NYC_Dog_Licensing_Dataset.csv')
+iq = pd.read_csv('dogdata/dog_intelligence.csv')
+nyc_census = pd.read_csv('censusdata/ACS_16_1YR_S0201_with_ann-edit.csv') # use 2016 data
+edmonton_registry = pd.read_csv('dogdata/Edmonton_Pet_Licenses_by_Neighbourhood-edit.csv')
+adelaide_registry = pd.read_csv('dogdata/Dog_Registrations_Adelaide_2016.csv')
+
+# Make the census columns understandable
+val = 'EST_'
+err = 'MOE_'
+age_under_5 = 'VC16'
+age_5_17 = 'VC17'
+age_18_24 = 'VC18'
+age_25_34 = 'VC19'
+age_35_44 = 'VC20'
+age_45_54 = 'VC21'
+age_55_64 = 'VC22'
+age_65_74 = 'VC23'
+age_75_over = 'VC24'
+
+pprint(nyc_registry.head())
+pprint(nyc_census.head())
+pprint(iq.head())
+pprint(edmonton_registry.head())
+pprint(adelaide_registry.head())
---
+++ notes.md
@@ -0,0 +1,4 @@
+# Project Notes
+
+Staten Island data is unavailable
+Male data was used for dog height and weight
