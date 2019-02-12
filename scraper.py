3cb07e2e851a805e07cd7acbe9da3599c99fc446
Michael <msuttles@gmail.com>
2019-02-11 20:48:26 -0600

    Progress on scraper

--- scraper.py
+++ scraper.py
@@ -1,8 +1,10 @@
 from pymongo import MongoClient
-from bs4 import BeautifulSoup
 import requests
 import time
 from pandas.io.html import read_html
+from pprint import pprint
+import sys
+import cython
 
 client = MongoClient()
 # Access/Initiate Database
@@ -11,10 +13,12 @@ db = client['dog_db']
 collection = db['pages']
 
 counter = 0
+categories= ['Origin', 'Color', 'Litter size', 'Coat', 'AKC', 'FCI', 'ANKC', 'CKC', 'KC (UK)', 'NZKC', 'UKC']
 
-
-with open('dogdata/urls2.txt') as f:
+with open('dogdata/urls.txt') as f:
     lst = f.read().splitlines() 
+
+lst = lst[10:15]
 
 for url in lst:
     print(url)
@@ -22,12 +26,31 @@ for url in lst:
     try:
         infobox = read_html(url, index_col=0, attrs={"class":"infobox biota"})
     except:
-        print "Some error"
-    print(infobox)
+        print("Error: " , sys.exc_info()[0])
+        continue
+    for cat in categories:
+        try:
+            pprint(cat)
+            if (cat == 'Color'):
+                pass
+            elif (cat == 'Weight'):
+                pprint(infobox[0].xs('Weight').values[0][1])
+            elif (cat == 'Height'):
+                pprint(infobox[0].xs('Height').values[0][1])
+            elif (cat == 'Life span'):
+                pprint(infobox[0].xs('Life span').values)
+            else:
+                if type(infobox[0].xs(cat).values[0]) == str:
+                    pprint(infobox[0].xs(cat).values[0])
+                else:
+                    pprint(infobox[0].xs(cat).values[0][0])
+        except:
+            print("Error: " , sys.exc_info()[0])
+            continue
+
+
     if (counter %5 == 0):
         time.sleep(6)
 f.close()
 
-
-    # soup = BeautifulSoup(page, 'html.parser')
-    # db.insert_one(requests.get({'url': url))
+# Litter size isn't working
