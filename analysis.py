883c077f10ebd06aa5033413ab038cd2ef95d75a
Michael <msuttles@gmail.com>
2019-02-12 14:18:32 -0600

    Progress on analysis code

--- analysis.py
+++ analysis.py
@@ -4,38 +4,43 @@ from pprint import pprint
 import sys
 import pandas as pd
 from fuzzywuzzy import fuzz
+import re
 # from fuzzywuzzy import process
 
+# Convert mongo to a DataFrame
 client = MongoClient('localhost', 27017)
 # Access/Initiate Database
 db = client['dogs']
 # Access/Initiate Table
 data = db['data']
+#convert entire collection to Pandas dataframe
+wiki = pd.DataFrame(list(data.find()))
 
-nyc_registry = pd.read_csv('dogdata/NYC_Dog_Licensing_Dataset.csv')
+nyc_registry = pd.read_csv('dogdata/NYC_Dog_Licensing_Dataset_2016-edit.csv')
 iq = pd.read_csv('dogdata/dog_intelligence-edit.csv')
 nyc_census = pd.read_csv('censusdata/ACS_16_1YR_S0201_with_ann-edit.csv') # use 2016 data
 edmonton_registry = pd.read_csv('dogdata/Edmonton_Pet_Licenses_by_Neighbourhood_2018-edit.csv')
-adelaide_registry = pd.read_csv('dogdata/Dog_Registrations_Adelaide_2016.csv')
+adelaide_registry = pd.read_csv('dogdata/Dog_Registrations_Adelaide_2016-edit.csv')
 
 # Make the census columns understandable
 val = 'EST_'
 err = 'MOE_'
 age_under_5 = 'VC16'
-age_5_17 = 'VC17'
-age_18_24 = 'VC18'
-age_25_34 = 'VC19'
-age_35_44 = 'VC20'
-age_45_54 = 'VC21'
-age_55_64 = 'VC22'
-age_65_74 = 'VC23'
-age_75_over = 'VC24'
+age_5_17 = 'VC17'; age_18_24 = 'VC18'
+age_25_34 = 'VC19'; age_35_44 = 'VC20'
+age_45_54 = 'VC21'; age_55_64 = 'VC22'
+age_65_74 = 'VC23'; age_75_over = 'VC24'
+
+# Sometimes centimeters are first and sometimes inches are first
+# wiki['Height'] = wiki['Height'].apply(lambda x: re.search(r'\d+', x).group())
+
 
 pprint(nyc_registry.head())
 pprint(nyc_census.head())
 pprint(iq.head())
 pprint(edmonton_registry.head())
 pprint(adelaide_registry.head())
+pprint(wiki.head())
 
 
 # fuzz.ratio(
--- notes.md
+++ notes.md
@@ -4,3 +4,6 @@ Staten Island data is unavailable
 Male data was used for dog height and weight
 For lowest category of dog intelligence, "obey" was not specified. I am assigning a value of 0.10
 Edmonton had almost no data for 2016 and very little for 2017, so I'm using 2018 data
+Removing "miniature" from the dataset.
+For further research, someone could use the census tract data in the NYC dataset.
+Extracting the first number from the height/weight for simplicity.
