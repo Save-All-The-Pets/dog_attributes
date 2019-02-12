7a306f297a6f9edf0ae016b0b9bc233230367693
Michael <msuttles@gmail.com>
2019-02-11 21:49:49 -0600

    Updates to scraper; worked on urls.txt

--- scraper.py
+++ scraper.py
@@ -4,7 +4,6 @@ import time
 from pandas.io.html import read_html
 from pprint import pprint
 import sys
-import cython
 
 client = MongoClient()
 # Access/Initiate Database
@@ -13,18 +12,21 @@ db = client['dog_db']
 collection = db['pages']
 
 counter = 0
-categories= ['Origin', 'Color', 'Litter size', 'Coat', 'AKC', 'FCI', 'ANKC', 'CKC', 'KC (UK)', 'NZKC', 'UKC']
+#Capturing data for male dogs 
+categories= ['Origin', 'Height', 'Weight', 'Color', 'Coat', 'AKC', 'FCI', 'ANKC', 'CKC', 'KC (UK)', 'NZKC', 'UKC']
 
 with open('dogdata/urls.txt') as f:
     lst = f.read().splitlines() 
 
-lst = lst[10:15]
+lst = lst[30:35]
 
 for url in lst:
     print(url)
     counter += 1
     try:
         infobox = read_html(url, index_col=0, attrs={"class":"infobox biota"})
+        dog_breed = url.replace('https://en.wikipedia.org/wiki/','').replace('_(dog)', '').replace('_',' ')
+        pprint(dog_breed)
     except:
         print("Error: " , sys.exc_info()[0])
         continue
@@ -32,13 +34,13 @@ for url in lst:
         try:
             pprint(cat)
             if (cat == 'Color'):
-                pass
+                pprint(infobox[0].xs('Color').values[0][0])
             elif (cat == 'Weight'):
                 pprint(infobox[0].xs('Weight').values[0][1])
             elif (cat == 'Height'):
                 pprint(infobox[0].xs('Height').values[0][1])
-            elif (cat == 'Life span'):
-                pprint(infobox[0].xs('Life span').values)
+            # elif (cat == 'Life span'):
+            #     pprint(infobox[0].xs('Life span').values[0])
             else:
                 if type(infobox[0].xs(cat).values[0]) == str:
                     pprint(infobox[0].xs(cat).values[0])
@@ -48,9 +50,10 @@ for url in lst:
             print("Error: " , sys.exc_info()[0])
             continue
 
-
     if (counter %5 == 0):
         time.sleep(6)
 f.close()
 
-# Litter size isn't working
+# Litter size, life span isn't working because they're two words
+
+# TO DO: Export names with the URLs, import them here
