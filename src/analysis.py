f5e03a73e65de287fdceacc3dff6d25bdbba40ee
Michael <msuttles@gmail.com>
2019-02-15 11:34:52 -0600

    Further functionalizing

--- src/analysis.py
+++ src/analysis.py
@@ -6,15 +6,21 @@ import sys
 from fuzzywuzzy import process
 import matplotlib.pyplot as plt
 import itertools
-# import plotly
-# import plotly.plotly as py
-# import plotly.graph_objs as go
- 
-# f = open('key.pem', 'r')
-# access_token = f.readline().strip('\n')
-# plotly.tools.set_credentials_file(username='msuttles', api_key=access_token)
-# plotly.tools.set_config_file(world_readable=False,
-#                              sharing='private')
+
+def describe(df, place, filename=None, display=True):
+    print('\n'+place+' Mean')
+    attrib_mean = df[lst].mean()
+    print(attrib_mean.round(decimals=2))
+    print('\n'+place+' Standard Deviation')
+    print(attrib[lst].std().round(decimals=2))
+
+    plt.title(place+' Overall Attributes')
+    plt.bar(attrib_mean.index, attrib_mean.values)
+    plt.ylim([0,0.65])
+    if filename is not None:
+        plt.savefig('../plots/'+filename)
+    if display:
+        plt.show()
 
 nyc_registry = pd.read_csv('../dogdata/NYC_Dog_Licensing_Dataset_2016-edit.csv')
 coren = pd.read_csv('../dogdata/coren-edit.csv')
@@ -24,34 +30,36 @@ adelaide_registry = pd.read_csv('../dogdata/Dog_Registrations_Adelaide_2016-edit
 wiki = pd.read_csv('../dogdata/wiki-edit.csv')
 turcsan = pd.read_csv('../dogdata/turcsan.csv')
 
-coren = coren[['Breed', 'Obedient']]
-attrib = coren.set_index('Breed').join(turcsan.set_index('Breed'), how='outer')
-
 lst = ['Bold','Calm', 'Obedient','Sociable', 'Trainable']
 
-wiki_breeds = set(wiki['Breed'].tolist())
-coren_breeds = set(coren['Breed'].tolist())
-turcsan_breeds = set(turcsan['Breed'].tolist())
+def describe_breeds(save=False):
+    coren = coren[['Breed', 'Obedient']]
+    attrib = coren.set_index('Breed').join(turcsan.set_index('Breed'), how='outer')
 
-print('\nIntersection, Wiki and Turcsan')
-print(wiki_breeds & turcsan_breeds)
-print('\nIntersection, Wiki and Coren')
-print(wiki_breeds & coren_breeds)
+    wiki_breeds = set(wiki['Breed'].tolist())
+    coren_breeds = set(coren['Breed'].tolist())
+    turcsan_breeds = set(turcsan['Breed'].tolist())
 
-print('\nDifference, Wiki - Turcsan')
-print(wiki_breeds - turcsan_breeds)
-print('\nDifference, Turcsan - Wiki')
-print(turcsan_breeds - wiki_breeds)
-print('\nDifference, Wiki - Coren')
-print(wiki_breeds - coren_breeds)
-print('\nDifference, Coren - Wiki')
-print(coren_breeds - wiki_breeds)
+    print('\nIntersection, Wiki and Turcsan')
+    print(wiki_breeds & turcsan_breeds)
+    print('\nIntersection, Wiki and Coren')
+    print(wiki_breeds & coren_breeds)
+    print('\nDifference, Wiki - Turcsan')
+    print(wiki_breeds - turcsan_breeds)
+    print('\nDifference, Turcsan - Wiki')
+    print(turcsan_breeds - wiki_breeds)
+    print('\nDifference, Wiki - Coren')
+    print(wiki_breeds - coren_breeds)
+    print('\nDifference, Coren - Wiki')
+    print(coren_breeds - wiki_breeds)
 
-# comparison = wiki[['Breed']]
-# comparison['Turcsan'] = comparison['Breed'].apply(lambda x: process.extractOne(x, list(turcsan_breeds)) if x not in turcsan_breeds else '')
-# comparison['Coren'] = comparison['Breed'].apply(lambda x: process.extractOne(x, list(coren_breeds)) if x not in coren_breeds else '')
-# comparison.to_csv('dogdata/comparisons.csv')
+    if save:
+        comparison = wiki[['Breed']]
+        comparison['Turcsan'] = comparison['Breed'].apply(lambda x: process.extractOne(x, list(turcsan_breeds)) if x not in turcsan_breeds else '')
+        comparison['Coren'] = comparison['Breed'].apply(lambda x: process.extractOne(x, list(coren_breeds)) if x not in coren_breeds else '')
+        comparison.to_csv('../dogdata/comparisons.csv')
 
+describe_breeds()
 
 # Canine attributes by AKC Groupings
 wiki_akc = wiki[['Breed', 'AKC']]
@@ -65,11 +73,13 @@ pprint(akc_std)
 akc_count = akc_groups_attrib.groupby('AKC').count()
 pprint(akc_count)
 
+# describe(akc_groups_attrib, 'AKC', display=False)
+
 
 def plot_by_attrib(df, categ,label, filename=None, display=True):
     fig, ax = plt.subplots()
     plt.setp(ax.xaxis.get_majorticklabels(), rotation=45, ha='right', rotation_mode='anchor')
-    for item in lst:
+    for item in categ:
         ax.plot(df.index, df[item])
 
     plt.gcf().subplots_adjust(bottom=0.3, right=.75)
@@ -89,24 +99,24 @@ nyc_registry['Borough'] = nyc_registry['Borough'].map(lambda x: None if x not in
 nyc_registry['BreedName'] = nyc_registry['BreedName'].map(lambda x: None if x == 'Unknown' else x)
 nyc_registry.dropna(inplace=True)
 
+# Display breed attributes by NYC borough
+
 nyc_attrib = nyc_registry.set_index('BreedName').join(attrib, how='left')
-#nyc_attrib_t = nyc_registry.join(turcsan, how='left')
 nyc_attrib = nyc_attrib[['Borough','Calm', 'Trainable', 'Sociable', 'Bold', 'Obedient']]
-nyc_attrib_g = nyc_attrib.groupby('Borough')
-print('\nNYC Mean:')
-nyc_attrib_mean = nyc_attrib_g.mean()
-pprint(nyc_attrib_mean.round(decimals=2))
-print('\nNYC Standard Deviation:')
-pprint(nyc_attrib_g.std().round(decimals=2))
-print('\nNYC Count:')
-pprint(nyc_attrib_g.count())
 
 
+def dogs_by_borough(n=False, display=True):
+    nyc_attrib_g = nyc_attrib.groupby('Borough')
+    print('\nNYC Mean:')
+    nyc_attrib_g_mean = nyc_attrib_g.mean()
+    pprint(nyc_attrib_g_mean.round(decimals=2))
+    print('\nNYC Standard Deviation:')
+    pprint(nyc_attrib_g.std().round(decimals=2))
+    print('\nNYC Count:')
+    pprint(nyc_attrib_g.count())
+    if display:
+        plot_by_attrib(nyc_attrib_g_mean, lst, 'NYC Borough')
 
-plot_by_attrib(nyc_attrib_mean, lst, 'NYC Borough')
-
-
-def dogs_by_borough(n=False):
     nyc_breeds = nyc_registry[['Borough', 'BreedName']]
     print('\nTop Dogs Count')
     pprint(nyc_breeds['BreedName'].value_counts().nlargest(5))
@@ -142,31 +152,16 @@ for i in chi2_breed_com:
     print('P-value: {}'.format(nyc_chi2[1].round(4)))
 
 
-def describe_city(df, place, filename=None, display=True):
-    if display:
-        print('\n'+place+' Mean')
-        attrib_mean = df[lst].mean()
-        print(attrib_mean.round(decimals=2))
-        print('\n'+place+' Standard Deviation')
-        print(attrib[lst].std().round(decimals=2))
-
-    plt.title(place+' Overall Attributes')
-    plt.bar(attrib_mean.index, attrib_mean.values)
-    plt.ylim([0,0.65])
-    if filename is not None:
-        plt.savefig('../plots/'+filename)
-    if display:
-        plt.show()
 
 
 
-describe_city(nyc_attrib, 'NYC')
+describe(nyc_attrib, 'NYC')
 
 adelaide_attrib = adelaide_registry.set_index('AnimalBreed').join(attrib, how='left')
-describe_city(adelaide_attrib, 'Adelaide')
+describe(adelaide_attrib, 'Adelaide')
 
 edmonton_attrib = edmonton_registry.set_index('BREED').join(attrib, how='left')
-describe_city(edmonton_attrib, 'Edmonton')
+describe(edmonton_attrib, 'Edmonton')
 
 
 
@@ -186,22 +181,7 @@ print(ancestral_uk_ire_grp.std().round(decimals=2))
 print('\nUK and Ireland Count')
 print(ancestral_uk_ire_grp.count())
 
-fig, ax = plt.subplots()
-plt.xticks(rotation=45)
-ax.plot(ancestral_uk_ire_mean.index, ancestral_uk_ire_mean['Bold'])
-ax.plot(ancestral_uk_ire_mean.index, ancestral_uk_ire_mean['Calm'])
-ax.plot(ancestral_uk_ire_mean.index, ancestral_uk_ire_mean['Obedient'])
-ax.plot(ancestral_uk_ire_mean.index, ancestral_uk_ire_mean['Sociable'])
-ax.plot(ancestral_uk_ire_mean.index, ancestral_uk_ire_mean['Trainable'])
-
-plt.setp(ax.xaxis.get_majorticklabels(), rotation=45, ha='right', rotation_mode='anchor') 
-plt.gcf().subplots_adjust(bottom=0.3, right=.75)
-plt.legend(loc=(1.04,0.6))
-plt.title('Scores by UK and Irish Ancestry')
-ax.set_ylabel('Score')
-ax.set_xlabel('Territory')
-plt.savefig('plots/uk_ireland.png')
-plt.show()
+plot_by_attrib(ancestral_uk_ire_mean, lst, 'UK and Irish Ancestry')
 
 # Combining Scotland, Wales, and England as United Kingdom
 ancestral['Origin'] = ancestral['Origin'].map(lambda x: 'United Kingdom' if x in {'England', 'Wales', 'Scotland'} else x)
@@ -246,20 +226,5 @@ pprint(ancestral_attrib_grp.std().round(decimals=2).dropna())
 print('\nAncestral Origin Count')
 pprint(ancestral_attrib_count)
 
-fig, ax = plt.subplots()
-# pprint(ancestral_attrib_mean_filtered.index)
-plt.xticks(rotation=45)
-ax.plot(ancestral_attrib_mean_filtered.index, ancestral_attrib_mean_filtered['Bold'])
-ax.plot(ancestral_attrib_mean_filtered.index, ancestral_attrib_mean_filtered['Calm'])
-ax.plot(ancestral_attrib_mean_filtered.index, ancestral_attrib_mean_filtered['Obedient'])
-ax.plot(ancestral_attrib_mean_filtered.index, ancestral_attrib_mean_filtered['Sociable'])
-ax.plot(ancestral_attrib_mean_filtered.index, ancestral_attrib_mean_filtered['Trainable'])
 
-plt.setp(ax.xaxis.get_majorticklabels(), rotation=45, ha='right', rotation_mode='anchor') 
-plt.gcf().subplots_adjust(bottom=0.3, right=.75)
-plt.legend(loc=(1.04,0.6))
-plt.title('Scores by Country of Ancestry')
-ax.set_ylabel('Score')
-ax.set_xlabel('Country')
-# plt.savefig('plots/ancestry.png')
-plt.show()
+plot_by_attrib(ancestral_attrib_mean_filtered, lst, 'Country of Origin')
